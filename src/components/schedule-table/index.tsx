39fad8a3cf6e76a87482b98b7a5ac286d8cb55db
Christian Zirke <christian.zirke@clevertech.biz>
2023-12-22 20:43:28 -0300

    feat: desktop track

--- src/components/schedule-table/index.tsx
+++ src/components/schedule-table/index.tsx
@@ -1,39 +1,71 @@
-import { Schedule } from '@/data/tracks';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { styled } from '@mui/system';
 import { Box } from '@mui/material';
+
+import { Schedule } from '@/data/tracks';
 
 type Props = {
 	schedule: Schedule;
 }
 
+const StyledP = styled('p')({
+	textAlign: 'center',
+	margin: '0 auto',
+	width: '100%',
+});
+
 // não vamos usar table por ser mais complicado de deixar responsivo
 const ScheduleTable = ({ schedule }: Props) => {
+	const theme = useTheme();
+	const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+
 	return (
 		<Box
 			style={{ color: '#b26590', maxWidth: 1200, margin: '0 auto', display: 'flex', padding: '0.5em', rowGap: 10 }}
-			sx={{ flexDirection: { xs: 'column', lg: 'row' }, backgroundColor: '#EEEEEE' }}
+			sx={{ flexDirection: { xs: 'column' }, backgroundColor: '#EEEEEE' }}
 		>
 			<label style={{ textAlign: 'center', fontSize: '2em', textTransform: 'uppercase' }}>Trilha {schedule.name}</label>
 			<Box
 				sx={{
 					'p:not(:first-child)': {
-						borderTop: '1px solid #b26590'
+						borderTop: {
+							xs: '1px solid #b26590',
+							lg: 'none',
+						},
+						borderLeft: {
+							lg: '1px solid #b26590'
+						},
+						paddingLeft: {
+							lg: '100px',
+						},
 					},
 					'> p': {
 						padding: '0.5em',
 					},
+					display: 'flex',
+					flexDirection: {
+						xs: 'column',
+						lg: 'row',
+					},
 				}}
 				style={{ border: '2px solid ', backgroundColor: '#ffd6eb', textAlign: 'center', fontSize: '1.5em' }}
 			>
-				<p>Horário</p>
-				<p>Palestras</p>
+				<StyledP sx={{ maxWidth: { lg: '200px' } }}>Horário</StyledP>
+				<StyledP sx={{ flex: 1, paddingLeft: { lg: '100px' } }}>Palestras</StyledP>
 			</Box>
 			{
 				schedule.schedule.map((talk, index) => (
 					<Box
 						key={index}
+						sx={{
+							flexDirection: {
+								xs: 'column',
+								lg: 'row',
+							},
+						}}
 						style={{
 							display: 'flex',
-							flexDirection: 'column',
 							alignItems: 'center',
 							textAlign: 'center',
 							padding: '1em',
@@ -41,18 +73,42 @@ const ScheduleTable = ({ schedule }: Props) => {
 							boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.14), 0 0px 2px -5px rgba(0, 0, 0, 0.4)',
 						}}
 					>
-						<p style={{ padding: 10, fontSize: '0.8em', fontWeight: 'bold' }}>{talk.from} - {talk.to}</p>
-						<p style={{ fontWeight: 'bold' }}>{talk.title}</p>
 						{
-							talk.speaker
-								? (
-									<Box style={{ marginTop: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-										<a style={{ fontWeight: 'bold' }} href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.name}</a>
-										<img alt={talk.speaker.name} src={talk.speaker.avatar} width="50%" style={{ borderRadius: 5, margin: '5px 0' }} />
-										<a href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.role}</a>
+							isDesktop ?	(
+								<>
+									<StyledP style={{ padding: 10, fontWeight: 'bold' }} sx={{ maxWidth: '200px', fontSize: { xs: '0.8em', lg: '1em' } }}>{talk.from} - {talk.to}</StyledP>
+									{talk.speaker && <img alt={talk.speaker.name} src={talk.speaker.avatar} width="100px" style={{ borderRadius: 5, margin: '5px 50px' }} />}
+									<Box style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', paddingLeft: talk.speaker ? 0 : '100px' }}>
+										<StyledP style={{ fontWeight: 'bold' }}>{talk.title}</StyledP>
+										{
+											talk.speaker
+												? (
+														<>
+															<a style={{ fontWeight: 'bold' }} href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.name}</a>
+															<a href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.role}</a>
+														</>
+												)
+												: null
+										}
 									</Box>
-								)
-								: null
+								</>
+							) : (
+								<>
+									<StyledP style={{ padding: 10, fontWeight: 'bold' }} sx={{ maxWidth: '200px', fontSize: { xs: '0.8em', lg: '1em' } }}>{talk.from} - {talk.to}</StyledP>
+									<StyledP style={{ fontWeight: 'bold' }}>{talk.title}</StyledP>
+									{
+										talk.speaker
+											? (
+												<Box style={{ marginTop: '25px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+													<a style={{ fontWeight: 'bold' }} href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.name}</a>
+													<img alt={talk.speaker.name} src={talk.speaker.avatar} width="50%" style={{ borderRadius: 5, margin: '5px 0' }} />
+													<a href={talk.speaker.link} rel="noreferrer" target="_blank">{talk.speaker.role}</a>
+												</Box>
+											)
+											: null
+									}
+								</>
+							)
 						}
 					</Box>
 				))
